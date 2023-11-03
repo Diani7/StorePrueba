d4891ee4d953b030157f1c0af3e43c6da08cf9cc
Diana Cifuentes <dimctes@gmail.com>
2023-11-02 23:35:48 -0500

    added users seeds

--- seeders/index.js
+++ seeders/index.js
@@ -1,14 +1,27 @@
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-      username: 'dimctes',
-      password: '123456',
-      role: 'admin',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
+        username: 'dimctes',
+        password: '123456',
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'cliente 1',
+        password: '123456',
+        role: 'customer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'cliente 2',
+        password: '654321',
+        role: 'customer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
   ]);
   },
   down: (queryInterface, Sequelize) => {
--- src/index.js
+++ src/index.js
@@ -47,8 +47,8 @@ app.use(cors());
     PurchaseModel.belongsToMany(ProductModel, { through: PurchaseProductsModel });
     ProductModel.belongsToMany(PurchaseModel, { through: PurchaseProductsModel });
 
-    await sequelizeInstance.sync({ force: true });
-    // await sequelizeInstance.authenticate();
+    // await sequelizeInstance.sync({ force: true });
+    await sequelizeInstance.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
